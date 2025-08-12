9aaa1fddcb6dc80d99ab80e9ac3a7ed32e469dd5
HenriqueSAP <heecs2014@gmail.com>
2025-08-12 13:51:31 -0400

    interfaces criadas com as funcionalidades de postagens

---
+++ src/repositories/post/json-post-repository.ts
@@ -0,0 +1,35 @@
+import { resolve } from 'path';
+import { PostRepository } from './post-repository';
+import { readFile } from 'fs/promises';
+import { PostModel } from '@/models/post/post-model';
+
+const ROOT_DIR = process.cwd();
+const JSON_POSTS_FILE_PATH = resolve(
+  ROOT_DIR,
+  'src',
+  'db',
+  'seed',
+  'post.json',
+);
+
+export class JsonPostRepository implements PostRepository {
+  private async readFromDisk() {
+    const jsonContent = await readFile(JSON_POSTS_FILE_PATH, 'utf-8');
+    const parsedJson = JSON.parse(jsonContent);
+    const { posts } = parsedJson;
+    return posts;
+  }
+
+  async findAll(): Promise<PostModel[]> {
+    return this.readFromDisk();
+  }
+
+  async findById(id: string): Promise<PostModel> {
+    const posts = await this.findAll();
+    const post = posts.find(post => post.id === id);
+    if (!post) throw new Error('Post não encontrado');
+    return post;
+  }
+}
+
+export const postRepository: PostRepository = new JsonPostRepository();
