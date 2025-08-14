4eca6de09bd13e4deeba0862b4fa3c9b12c61c5d
HenriqueSAP <heecs2014@gmail.com>
2025-08-13 21:02:41 -0400

    feat: featurePost Tailwind Css

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,12 +2,31 @@ import { Container } from '@/components/Container';
 import { Header } from '@/components/Header';
 import { PostsList } from '@/components/PostsList';
 import { SpinLoader } from '@/components/SpinLoader';
+import Image from 'next/image';
+import Link from 'next/link';
 import { Suspense } from 'react';
 
 export default async function HomePage() {
   return (
     <Container>
       <Header />
+      <section className='grid grid-cols-1 gap-8 mb-16 sm:grid-cols-2 group'>
+        <Link className='w-full h-full overflow-hidden rounded-xl' href='#'>
+          <Image
+            className='group-hover:scale-105 transition'
+            src='/images/bryen_0.png'
+            width={1200}
+            height={720}
+            alt='Título do post'
+          />
+        </Link>
+        <div>
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Asperiores
+          illo enim porro numquam cum dicta, veritatis quas minus rerum magnam
+          repudiandae esse et adipisci optio obcaecati corporis necessitatibus
+          ratione quae.
+        </div>
+      </section>
       <Suspense fallback={<SpinLoader />}>
         <PostsList />
       </Suspense>
