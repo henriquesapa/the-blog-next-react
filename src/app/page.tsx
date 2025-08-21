286eec22074ebd4a0d8049e759a235b3022c796d
HenriqueSAP <hsilvaaparecido@gmail.com>
2025-08-21 16:26:19 -0300

    Movendo PostFeature para um componente separado

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,6 @@
 import { Container } from '@/components/Container';
 import { Header } from '@/components/Header';
-import { PostCoverImage } from '@/components/PostCoverImage';
-import { PostHeading } from '@/components/PostHeading';
+import { PostFeatured } from '@/components/PostFeatured';
 import { PostsList } from '@/components/PostsList';
 import { SpinLoader } from '@/components/SpinLoader';
 import { Suspense } from 'react';
@@ -10,37 +9,17 @@ export default async function HomePage() {
   return (
     <Container>
       <Header />
-      <section className='grid grid-cols-1 gap-8 mb-16 sm:grid-cols-2 group'>
-        <PostCoverImage
-          linkProps={{ href: '/post/asdf' }}
-          imageProps={{
-            width: 1200,
-            height: 720,
-            src: '/images/bryen_9.png',
-            alt: 'Alt da imagem',
-            priority: true,
-          }}
-        />
-        <div className='flex flex-col gap-4 sm:justify-center'>
-          <time className='text-slate-600 text-sm/tight' dateTime='2025-04-20'>
-            20/04/2025 10:00
-          </time>
-          <PostHeading as='h1' url='#'>
-            Et adipisci optio obcaecati corporis necessitatibus ratione quae
-          </PostHeading>
-          <p>
-            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Asperiores
-            illo enim porro numquam cum dicta, veritatis quas minus rerum magnam
-            repudiandae esse et adipisci optio obcaecati corporis necessitatibus
-            ratione quae.
-          </p>
-        </div>
-      </section>
+
+      <Suspense fallback={<SpinLoader />}>
+        <PostFeatured />
+      </Suspense>
+
       <Suspense fallback={<SpinLoader />}>
         <PostsList />
       </Suspense>
+
       <footer>
-        <p className='text-6xl font-bold text-center py-8'> Footer</p>
+        <p className='text-6xl font-bold text-center py-8'>Footer</p>
       </footer>
     </Container>
   );
--- src/components/PostsList/index.tsx
+++ src/components/PostsList/index.tsx
@@ -4,13 +4,18 @@ import { PostHeading } from '../PostHeading';
 
 export async function PostsList() {
   const posts = await postRepository.findAll();
+
   return (
-    <div className='grid grid-cols-1 gap-8 sm:grid-cols-2 md:grid-cols-3'>
+    <div className='grid grid-cols-1  gap-8 sm:grid-cols-2 lg:grid-cols-3'>
       {posts.map(post => {
+        const postLink = `/post/${post.slug}`;
+
         return (
           <div className='flex flex-col gap-4 group' key={post.id}>
             <PostCoverImage
-              linkProps={{ href: `/post/${post.slug}` }}
+              linkProps={{
+                href: postLink,
+              }}
               imageProps={{
                 width: 1200,
                 height: 720,
@@ -19,16 +24,18 @@ export async function PostsList() {
               }}
             />
 
-            <div className='flex flex-col gap-4 group sm:justify-center'>
+            <div className='flex flex-col gap-4 sm:justify-center'>
               <time
-                className='text-slate-600 text-sm/tight'
+                className='text-slate-600 block text-sm/tight'
                 dateTime={post.createdAt}
               >
                 {post.createdAt}
               </time>
-              <PostHeading as='h2' url='#'>
+
+              <PostHeading as='h2' url={postLink}>
                 {post.title}
               </PostHeading>
+
               <p>{post.excerpt}</p>
             </div>
           </div>
