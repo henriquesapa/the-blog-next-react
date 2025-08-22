9114d0b0ad482411bf29ba627880bd335db67b0c
HenriqueSAP <hsilvaaparecido@gmail.com>
2025-08-22 16:30:26 -0300

    Corrigindo o heading não destacado

--- src/components/PostHeading/index.tsx
+++ src/components/PostHeading/index.tsx
@@ -1,7 +1,8 @@
+import clsx from 'clsx';
 import Link from 'next/link';
 
 type PostHeadingProps = {
-  children?: React.ReactNode;
+  children: React.ReactNode;
   url: string;
   as?: 'h1' | 'h2';
 };
@@ -12,15 +13,15 @@ export function PostHeading({
   as: Tag = 'h2',
 }: PostHeadingProps) {
   const headingClassesMap = {
-    h1: 'text-2xl/tight sm:text-4xl extrabold',
-    h2: 'text-2xl/tight font-bold ',
+    h1: 'text-2xl/tight sm:text-4xl font-extrabold',
+    h2: 'text-2xl/tight font-bold',
   };
 
   const commonClasses = '';
 
   return (
-    <Tag className={(headingClassesMap[Tag], commonClasses)}>
-      <Link className='group-hover:text-shadow-slate-600 transition' href={url}>
+    <Tag className={clsx(headingClassesMap[Tag], commonClasses)}>
+      <Link className='group-hover:text-slate-600 transition' href={url}>
         {children}
       </Link>
     </Tag>
